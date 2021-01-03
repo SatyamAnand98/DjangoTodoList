cb4b5af67eec16a4939cf252301e90e40b48dbd4
SatyamAnand98 <thesatemail@gmail.com>
2021-01-03 13:01:19 +0530

    overdue added

--- venv/sampleproject/todolist/views.py
+++ venv/sampleproject/todolist/views.py
@@ -6,11 +6,22 @@ from rest_framework.response import Response
 from rest_framework import status
 from . models import task
 from . serializers import todoListSerializer
+from django.utils import timezone
+
 
 class todoList(APIView):
     def get(self, request):
+        now = timezone.now()
         task1 = task.objects.all()
         searializer = todoListSerializer(task1, many = True)
+        for e in task1:
+            if not e.complete:
+                if e.due<now:
+                    e.overdue=True
+                    e.save()
+                else:
+                    e.overdue=False
+                    e.save()
         return(Response(searializer.data))
 
     def post(self, request):
@@ -23,4 +34,11 @@ class todoList(APIView):
         )
         # if form.is_valid():
         #     text = form.cleaned_data['post']
+        return(Response(request.data))
+
+    def put(self, request):
+        data = task.objects.get(id=request.data['id'])
+        # data = request.data
+        data.complete=request.data['complete']
+        data.save()
         return(Response(request.data))
